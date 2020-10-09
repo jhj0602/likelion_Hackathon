ce854a59aa04e41813071d4ef3c20f049960bd7f
Juyoung Park <wndud5570@naver.com>
2020-10-09 18:52:11 +0900

    mainpage 수정

--- deeplearning/views.py
+++ deeplearning/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten,Conv2D, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator,array_to_img, img_to_array, load_img
-from tensorflow.keras import optimizers, initializers, regularizers, metrics
-from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
-import os
-from PIL import Image
-# Create your views here.
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import tensorflow as tf
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Activation, Dropout, Flatten,Conv2D, MaxPooling2D
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator,array_to_img, img_to_array, load_img
+# from tensorflow.keras import optimizers, initializers, regularizers, metrics
+# from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
+# import os
+# from PIL import Image
+# # Create your views here.
 def learning(request):
     return
